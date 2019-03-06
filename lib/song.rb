0ad4678ad01abe84edb2e5b87b114b76ef326c48
Brian Simonton <bsimonton78@gmail.com>
2019-03-06 02:37:58 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -58,7 +58,14 @@ def self.alphabetical
   
 end
 
-
+def self.new_from_filename(filename)
+    song_array = filename.split(" - ")
+    song_array[1] = song_array[1].chomp(".mp3")
+    song = self.new
+    song.name = song_array[1]
+    song.artist_name = song_array[0]
+    song
+  end
 
 
 
